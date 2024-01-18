554b1bd41833cc2142afd72248696fbc91dab1ad
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-18 19:35:50 +0530

    Remove the `helloWorld` starter command

--- src/extension.ts
+++ src/extension.ts
@@ -6,21 +6,25 @@ import * as vscode from 'vscode';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-csv" is now active!');
+	vscode.window.showInformationMessage('CSV Extension is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-csv.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-csv!');
-	});
+	if (vscode.window.activeTextEditor?.document) {
+		const document = vscode.window.activeTextEditor.document;
+		if (document.fileName.endsWith(".csv")) {
+			console.log(parseCsv(document.getText()));
+		}
+	}
 
-	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
+
+/**
+ * Parses a csv document into a 2D array
+ * @param doc The csv document to parse
+ * @returns A 2D array of the csv document
+ */
+function parseCsv(doc: string): string[][] {
+	return [["Hello World!"]];
+}
