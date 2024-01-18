dd3c1ac9133a1ea01611afc9d7f04ed794bd866f
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-18 20:07:34 +0530

    Extract `parseCsv` function out to its own file

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { parseCsv } from './csv';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -19,24 +20,3 @@ export function activate(context: vscode.ExtensionContext) {
 
 // This method is called when your extension is deactivated
 export function deactivate() { }
-
-/**
- * Parses a csv document into a 2D array
- * @param doc The csv document to parse
- * @returns A 2D array of the csv document
- */
-function parseCsv(doc: string): string[][] {
-	/** The resulting 2D array */
-	let result: string[][] = [];
-
-	// Split the document into lines
-	const lines = doc.split("\n");
-
-	// Iterate over each line and split it into columns
-	for (const line of lines) {
-		result.push(line.split(","));
-	}
-
-	// Return the result
-	return result;
-}
