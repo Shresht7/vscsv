921f45b342f6fa48474b517de3e9616957fdc869
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-20 01:18:36 +0530

    Add symbol creation logic

--- src/symbols.ts
+++ src/symbols.ts
@@ -1,5 +1,6 @@
 // Library
 import * as vscode from 'vscode';
+import { VSCSV } from './library';
 
 // -------
 // SYMBOLS
@@ -36,11 +37,57 @@ export class DocumentSymbolProvider implements vscode.DocumentSymbolProvider {
     // INSTANCE
     // --------
 
+    private parser = new VSCSV();
+
     public provideDocumentSymbols(
         document: vscode.TextDocument,
         token: vscode.CancellationToken
     ): vscode.ProviderResult<vscode.SymbolInformation[] | vscode.DocumentSymbol[]> {
-        return [];
+
+        // Initialize the symbols array
+        const symbols: vscode.DocumentSymbol[] = [];
+
+        // Parse the CSV document
+        const csv = this.parser.parse(document.getText());
+
+        // Iterate over the rows in the CSV document...
+        for (const r in csv.data) {
+            const row = csv.data[r];
+            if (!row.length) { continue; } // Exit if the row is empty
+
+            // Create a symbol for the row and add it to the symbols array
+            const range = new vscode.Range(+r, 0, +r, Number.MAX_VALUE);
+            const rowSymbols: vscode.DocumentSymbol = new vscode.DocumentSymbol(
+                r,
+                document.getText(range),
+                vscode.SymbolKind.Array,
+                range,
+                range,
+            );
+
+            // Iterate over the cells in the row...
+            for (const c in row) {
+                const cell = row[c];
+                if (!cell.value) { continue; }
+
+                // Create a symbol for the cell and add it to the row symbol
+                const range = new vscode.Range(cell.line, cell.column, cell.line, cell.columnEnd);
+                const symbol = new vscode.DocumentSymbol(
+                    r + ":" + c + " " + csv.headers[c].value + ": " + cell.value,
+                    csv.headers[c].value || cell.value,
+                    vscode.SymbolKind.Field,
+                    range,
+                    range,
+                );
+                rowSymbols.children.push(symbol);
+            }
+
+            // Add the row symbol to the symbols array
+            symbols.push(rowSymbols);
+        }
+
+        // Return the symbols array
+        return symbols;
     }
 
 }
