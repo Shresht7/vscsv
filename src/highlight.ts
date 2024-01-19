7d9547492614ead435cc650e0cd85c94e11d8e3e
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-19 23:17:49 +0530

    Extract utility function out

--- src/highlight.ts
+++ src/highlight.ts
@@ -1,6 +1,7 @@
 // Library
 import * as vscode from 'vscode';
 import { VSCSV } from './library';
+import { shuffle } from './utils';
 
 // ---------
 // HIGHLIGHT
@@ -70,17 +71,3 @@ export class DocumentSemanticTokensProvider implements vscode.DocumentSemanticTo
     }
 }
 
-/**
- * Shuffle an array in place using the Fisher-Yates algorithm
- * @param array The array to shuffle
- * @returns The shuffled array
- */
-function shuffle<T>(array: T[]) {
-    for (let i = array.length - 1; i > 0; i--) {
-        // Pick a random index from 0 to i
-        const j = Math.floor(Math.random() * (i + 1));
-        // Swap array[i] with array[j]
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-}
