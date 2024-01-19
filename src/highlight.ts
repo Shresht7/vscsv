44c9bc68d55cb09f802c2eb0aac52b0c391b2d3e
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-19 23:10:40 +0530

    Create `shuffle` helper function
    to shuffle the elements of an array using Fisher-Yates algorithm

--- src/highlight.ts
+++ src/highlight.ts
@@ -16,12 +16,12 @@ export class DocumentSemanticTokensProvider implements vscode.DocumentSemanticTo
     private static disposable: vscode.Disposable;
 
     /** The semantic tokens legend used to map token types to colors and styles */
-    private static legend = new vscode.SemanticTokensLegend([
+    private static legend = new vscode.SemanticTokensLegend(shuffle([
         'nonexistent', // The first column is styled as regular text (not an actual token)
-        'comment', 'string', 'keyword', 'number', 'regexp', 'operator', 'namespace',
+        'string', 'number', 'regexp', 'operator', 'keyword', 'namespace', 'comment',
         'type', 'struct', 'class', 'interface', 'enum', 'typeParameter', 'function',
         'method', 'decorator', 'macro', 'variable', 'parameter', 'property', 'label'
-    ]);
+    ]));
 
     /**
     * Initialize the semantic tokens provider for the CSV language to provide syntax highlighting.
@@ -68,4 +68,19 @@ export class DocumentSemanticTokensProvider implements vscode.DocumentSemanticTo
         // Return the semantic tokens
         return builder.build();
     }
+}
+
+/**
+ * Shuffle an array in place using the Fisher-Yates algorithm
+ * @param array The array to shuffle
+ * @returns The shuffled array
+ */
+function shuffle<T>(array: T[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+        // Pick a random index from 0 to i
+        const j = Math.floor(Math.random() * (i + 1));
+        // Swap array[i] with array[j]
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
