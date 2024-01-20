068ca4236e16a4fab7ae0b8e887c17994652129c
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-20 20:02:24 +0530

    Create `HoverProvider`

---
+++ src/providers/hover.ts
@@ -0,0 +1,45 @@
+// Library
+import * as vscode from 'vscode';
+
+// --------------
+// HOVER PROVIDER
+// --------------
+
+export class HoverProvider implements vscode.HoverProvider {
+
+    // STATIC
+    // ------
+
+    /** The document selector for the CSV language */
+    private static selector: vscode.DocumentSelector = { language: 'csv' };
+
+    /** The disposable used to unregister this provider, when needed */
+    private static disposable: vscode.Disposable;
+
+    /**
+     * Initialize the hover provider for the CSV language to provide hover information.
+     * @param context The vscode extension context (see {@linkcode vscode.ExtensionContext})
+     * @see {@link vscode.languages.registerHoverProvider}
+     * @see {@link vscode.HoverProvider}
+     */
+    public static initialize(context: vscode.ExtensionContext) {
+        this.disposable?.dispose(); // Dispose of any existing provider
+        // Register the hover provider for the CSV language to provide hover information
+        this.disposable = vscode.languages.registerHoverProvider(this.selector, new HoverProvider());
+        // Register the disposable to dispose of the provider when the extension is deactivated
+        context.subscriptions.push(this.disposable);
+    }
+
+    /** Dispose of the hover provider. @see {@link vscode.Disposable} */
+    public static dispose() {
+        this.disposable?.dispose();
+    }
+
+    // INSTANCE
+    // --------
+
+    provideHover(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Hover> {
+        throw new Error('Method not implemented.');
+    }
+
+}
