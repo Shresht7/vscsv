cfe984951b4fee6333c17a5a312bbc3bb435fb30
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-23 17:02:38 +0530

    Extract `createRow` in to helpers

--- src/webview/scripts/index.ts
+++ src/webview/scripts/index.ts
@@ -1,42 +1,33 @@
+// Library
+import { createRow } from "./helpers";
+
 // DOM ELEMENTS
 const table = document.getElementById('table')! as HTMLTableElement;
 
-/**
- * Creates a table row element and adds it to the table
- * @param row The row to add to the table
- * @param isHeader The row is a header row
- */
-function createRow(row: string[], isHeader: boolean) {
-    // Create a table row element
-    const tr = document.createElement('tr');
-
-    // Determine the tag name to use
-    const tagName = isHeader ? 'th' : 'td';
-
-    // Loop over the cells in the row...
-    for (const cell of row) {
-        // ... and create a table cell element for each
-        const td = document.createElement(tagName);
-        td.textContent = cell;
-        td.style.padding = '0.35rem 0.5rem';
-        tr.appendChild(td);
-    }
-
-    // Add the row to the table
-    table.appendChild(tr);
-}
-
 // Listen for messages from the main thread and render the table 
 window.addEventListener('message', (event) => {
+
     const message = /** @type any[] */ (event.data);
     switch (message.command) {
+
         case 'update':
+
+            // Clear the table
             table.innerHTML = '';
+            /** A collection of table row elements to append to the table */
+            const tableRows: HTMLTableRowElement[] = [];
+
+            // Create the header row
             const headers = message.data.shift();
-            createRow(headers, true);
+            tableRows.push(createRow(headers, true));
+
+            // Create the data rows
             for (const row of message.data) {
-                createRow(row, false);
+                tableRows.push(createRow(row, false));
             }
+
+            // Append the rows to the table
+            table.append(...tableRows);
             break;
     }
 });
