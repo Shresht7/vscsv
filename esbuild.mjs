3a01f3045d20a98831ac968e74c3562e36968d37
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-23 12:08:40 +0530

    Switch to bundling with `esbuild`

---
+++ esbuild.mjs
@@ -0,0 +1,48 @@
+//@ts-check
+
+// Library
+import * as esbuild from 'esbuild';
+
+/** Boolean indicating whether the current build is for production. */
+const isProduction = process.env.NODE_ENV === 'production';
+
+/**
+ * Base configuration options for all builds.
+ * @type {import('esbuild').BuildOptions}
+*/
+const baseOptions = {
+    bundle: true,
+    minify: isProduction, // Minify in production
+    sourcemap: !isProduction, // Generate sourcemaps in development
+};
+
+/**
+ * Configuration for the extension source code. (to be run in the Node.js context)
+ * @type {import('esbuild').BuildOptions}
+ */
+const extensionOptions = {
+    ...baseOptions,
+    platform: 'node',
+    mainFields: ['module', 'main'],
+    format: 'cjs',
+    entryPoints: ['./src/extension.ts'],
+    outfile: './out/extension.js',
+    external: ['vscode'],
+};
+
+// Build the extension
+const args = process.argv.slice(2);
+const context = await esbuild.context(extensionOptions);
+try {
+    if (args.includes('--watch')) {
+        await context.watch();
+        console.log('Watching for changes...');
+    } else {
+        console.log('Building...');
+        await context.rebuild();
+        console.log('Build finished');
+    }
+} catch (err) {
+    process.stderr.write(err.message);
+    process.exit(1);
+}
