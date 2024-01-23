35d125b1ee586b73992979ec87e2ca37320ab540
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-23 22:00:46 +0530

    Update comments

--- src/extension.ts
+++ src/extension.ts
@@ -23,10 +23,10 @@ export function activate(context: vscode.ExtensionContext) {
 
 	vscode.window.showInformationMessage('CSV Extension is now active!'); // TODO: Remove this when the extension is ready
 
-	// Register the configuration listeners
+	// Register the configuration manager
 	Configuration.initialize(context);
 
-	// Register commands
+	// Register the extension commands
 	Commands.initialize(context);
 
 	// Register the diagnostics provider to provide diagnostics
@@ -35,13 +35,13 @@ export function activate(context: vscode.ExtensionContext) {
 	// Register the hover provider to provide hover information
 	HoverInformation.initialize(context);
 
-	// Register the document symbol provider to provide symbol information
+	// Register the document symbol provider to provide outline and go-to-symbol functionality
 	Symbols.initialize(context);
 
-	// Register the semantic tokens provider provide syntax highlighting
+	// Register the semantic tokens provider provide syntax-highlighting
 	SyntaxHighlighting.initialize(context);
 
-	// Register the webview provider to provide webview functionality
+	// Register the webview provider to provide table preview functionality
 	Webview.initialize(context);
 
 }
