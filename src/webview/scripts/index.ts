3b0440a4205d8a9a4a2517dffe76c9040f26f22d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-24 19:30:39 +0530

    Refactor table rendering logic in index.ts

--- src/webview/scripts/index.ts
+++ src/webview/scripts/index.ts
@@ -19,35 +19,21 @@ window.addEventListener('load', () => {
 // MAIN
 // ----
 
+/** The class representing the html table */
+const table = new Table('table');
+
+/** The main function */
 function main() {
-    const table = new Table('table');
-
-    function handleUpdate(payload: string[][]) {
-        // Clear the table
-        table.clear();
-
-        // Return early if there is no data
-        if (payload?.length < 1) { return; }
-
-        // Create the header row
-        const headers = payload.shift()!;
-        table.addHeaderRow(headers);
-
-        // Create the data rows
-        for (const row of payload) {
-            table.addDataRow(row);
-        }
-    }
-
     // Listen for messages from the main thread and render the table 
-    window.addEventListener('message', (event) => {
+    window.addEventListener('message', handleMessageEvent);
+}
 
-        const message = /** @type any[] */ (event.data);
-        switch (message.command) {
-
-            case 'update':
-                handleUpdate(message.data);
-                break;
-        }
-    });
+/** Message Event Handler */
+function handleMessageEvent(event: MessageEvent) {
+    const message = event.data;
+    switch (message.command) {
+        case 'update':
+            table.update(message.data);
+            break;
+    }
 }
--- src/webview/scripts/table.ts
+++ src/webview/scripts/table.ts
@@ -4,24 +4,36 @@
 
 export class Table {
 
+    /** The HTML table element */
     private element: HTMLTableElement;
 
+    /** Creates a new table */
     constructor(private readonly id: string) {
         this.element = document.getElementById(id) as HTMLTableElement;
     }
 
+    /** Clears the table */
     clear() {
         this.element.innerHTML = '';
     }
 
-    addHeaderRow(row: string[]) {
-        this.element.appendChild(this.createRow(row, 'th'));
-    }
+    /** Updates the table with the given data */
+    update(data: string[][]) {
+        this.clear();
 
-    addDataRow(row: string[]) {
-        this.element.appendChild(this.createRow(row, 'td'));
-    }
+        // Return early if there is no data
+        if (data?.length < 1) { return; }
 
+        // Create the header row
+        const headers = data.shift()!;
+        this.element.appendChild(this.createRow(headers, 'th'));
+
+        // Create the data rows
+        for (const row of data) {
+            this.element.appendChild(this.createRow(row, 'td'));
+        }
+    }
+    /** Creates a table row */
     private createRow(row: string[], tagName: 'th' | 'td' = 'td') {
         const tr = document.createElement('tr');
         for (const cell of row) {
@@ -30,10 +42,12 @@ export class Table {
         return tr;
     }
 
+    /** Creates a table cell */
     private createCell(cell: string, tagName: 'th' | 'td' = 'td') {
         const td = document.createElement(tagName);
         td.textContent = cell;
         td.style.padding = '0.35rem 0.5rem';
         return td;
     }
+
 }
