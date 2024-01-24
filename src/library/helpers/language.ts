1c3e7426e5123ae4e9e72925f620101d7660841e
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-24 20:14:56 +0530

    Fix issue with crash

--- src/library/helpers/language.ts
+++ src/library/helpers/language.ts
@@ -10,7 +10,7 @@ export type SupportedLanguage = typeof LANGUAGE[keyof typeof LANGUAGE];
 
 /** Determines if the language is supported by the extension */
 export function isSupported(id: string): id is SupportedLanguage {
-    return id in LANGUAGE;
+    return Object.values(LANGUAGE).includes(id as SupportedLanguage);
 }
 
 /** Maps the language (IDs) to their corresponding delimiters */
